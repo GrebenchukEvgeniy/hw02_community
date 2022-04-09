1e33af4daff1fb2cd1683a75771a944d9472b059
Евгений Гребенчук <grebenchuk.evgeniy@gmail.com>
2022-04-09 15:07:35 +0500

    It has done

---
+++ yatube/posts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+# Из модуля auth импортируем функцию get_user_model
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    # Тип: TextField
+    text = models.TextField()
+    # Тип поля: DateTimeField, для хранения даты и времени;
+    # параметр auto_now_add определяет, что в поле будет автоматически
+    # подставлено время и дата создания новой записи
+    pub_date = models.DateTimeField(auto_now_add=True)
+    # Тип: ForeignKey, ссылка на модель User
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.CASCADE,
+        blank=True, null=True
+    )
