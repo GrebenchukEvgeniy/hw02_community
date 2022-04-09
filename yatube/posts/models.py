ebe6baa1be442f9d268c45db925777c8d964049c
Евгений Гребенчук <grebenchuk.evgeniy@gmail.com>
2022-04-09 23:42:04 +0500

    after 3rd review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -16,7 +16,7 @@ class Group(models.Model):
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True,
-                                    verbose_name="publication date")
+                                    verbose_name='publication date')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
